332d844261985a00ebbf56984669ca321177aad9
Feiv7 <benjamingoncalves@hotmail.fr>
2022-04-01 10:31:01 +0200

    First Commit

---
+++ src/components/item/FilmItem.vue
@@ -0,0 +1,104 @@
+<template>
+	<div 
+		class="film-item-component"
+	>
+		<p>film-item-component</p>
+	</div>
+</template>
+
+<script>
+	/*
+		[VUE] Component
+		Define properties and methods => https://bit.ly/3GdqmXg
+	*/
+		export default {
+			// [VUE] Component name
+			name: 'FilmItem',
+
+			/*
+				[VUE] Components => https://bit.ly/3GdqmXg
+				Used to inject children components
+			*/
+				components: {},
+			//
+
+			/*
+				[VUE] Data => https://bit.ly/3GdqmXg
+				Used to inject data in the Vue.js component
+			*/
+				data(){
+					return { }
+				},
+			//
+
+			/*
+				[VUE] Methods => https://bit.ly/3GdqmXg
+				Used to add methods in Vue.js component
+			*/
+				methods:{ },
+			//
+
+			/*
+				[VUE] Hooks => https://bit.ly/3s7AwEa
+				Used for Vue.js component life cycle
+			*/
+				/* 
+					[HOOK] Created
+					Called synchronously after the instance is created
+				*/
+					created: async function(){},
+				//
+				
+
+				/* 
+					[HOOK] Mounted
+					Called after the instance has been mounted
+				*/
+					mounted: async function(){},
+				//
+
+				/* 
+					// Called synchronously after the instance has been initialized
+					created: async function(){},
+
+					// Called synchronously after the instance is created
+					created: async function(){},
+
+					// Called right before the mounting begins
+					beforeMount: async function(){},
+
+					// Called after the instance has been mounted
+					mounted: async function(){},
+
+					// Called when data changes
+					beforeUpdate: async function(){},
+
+					// Called after a data change
+					updated: async function(){},
+
+					// Called when a kept-alive component is activated
+					activated: async function(){},
+
+					// Called when a kept-alive component is deactivated
+					deactivated: async function(){},
+
+					// Called right before a component instance is unmounted
+					beforeUnmount: async function(){},
+
+					// Called after a component instance has been unmounted
+					unmounted: async function(){},
+
+					// Called when an error from any descendent component is captured
+					errorCaptured: async function(){},
+
+					// Called when virtual DOM re-render is tracked
+					renderTracked: async function(){},
+
+					// Called when virtual DOM re-render is triggered
+					renderTriggered: async function(){},
+				*/
+			//
+		}
+	//
+</script>
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+/* 
+	[VUE] Main imports
+	Define main imports to create the application
+*/
+    // Vue imports
+    import Vue from 'vue'; // Open Source do it better => https://vuejs.org
+
+    // App imports
+    import App from './App.vue'; // Main application controller
+    import AppRouter from "./router"; // Path binding => https://router.vuejs.org
+//
+
+
+/* 
+	[APP] Start Vue.js app
+	Init new Vue.js applilcation
+*/
+    // Set configuration	
+    Vue.config.productionTip = false;
+
+    // Instantiate new Vue.js Application
+    new Vue({
+        router: AppRouter,
+        render: create => create(App),
+    })
+    .$mount('#app');
+//
